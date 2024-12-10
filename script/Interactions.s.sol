780d3bc897181a99c5e31493e661f6ccb1e87097
MPTGits <mplamenovtz@gmail.com>
2024-12-11 01:17:44 +0200

    Ininital commit

---
+++ script/Interactions.s.sol
@@ -0,0 +1,115 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.19;
+
+import {Script, console} from "forge-std/Script.sol";
+import {HelperConfig, CodeConstants} from "script/HelperConfig.s.sol";
+import {VRFCoordinatorV2_5Mock} from "@chainlink/contracts/src/v0.8/vrf/mocks/VRFCoordinatorV2_5Mock.sol";
+import {LinkToken} from "test/mocks/LinkToken.sol";
+import {DevOpsTools} from "lib/foundry-devops/src/DevOpsTools.sol";
+
+contract CreateSubscription is Script {
+    function createSubscriptionUsingConfig() public returns (uint256, address) {
+        HelperConfig helperConfig = new HelperConfig();
+        address vrfCoordinator = helperConfig.getConfig().vrfCoordinator;
+        (uint256 subId, ) = createSubscription(vrfCoordinator);
+        return (subId, vrfCoordinator);
+    }
+
+    function createSubscription(
+        address vrfCoordinator
+    ) public returns (uint256, address) {
+        console.log(
+            "Creating a Chainlink VRF subscription on chain id: ",
+            block.chainid
+        );
+        vm.startBroadcast();
+        uint256 subId = VRFCoordinatorV2_5Mock(vrfCoordinator)
+            .createSubscription();
+        vm.stopBroadcast();
+        console.log("Subscription ID: ", subId);
+        console.log(
+            "Please update the subscription ID in the HelperConfig contract"
+        );
+        return (subId, vrfCoordinator);
+    }
+
+    function run() public {
+        // Create a Chainlink VRF subscription
+    }
+}
+
+contract FundSubscription is Script, CodeConstants {
+    uint public constant FUND_AMOUNT = 3 ether;
+
+    function fundSubscriptionUsingConfig() public {
+        HelperConfig helperConfig = new HelperConfig();
+        address vrfCoordinator = helperConfig.getConfig().vrfCoordinator;
+        uint256 subscriptionId = helperConfig.getConfig().subscriptionId;
+        address linkToken = helperConfig.getConfig().link;
+        fundSubscription(vrfCoordinator, subscriptionId, linkToken);
+    }
+
+    function fundSubscription(
+        address vrfCoordinator,
+        uint256 subscriptionId,
+        address linkToken
+    ) public {
+        console.log("Funding subscription:", subscriptionId);
+        console.log("Using vrfCoordinator:", vrfCoordinator);
+        console.log("On ChainId: ", block.chainid);
+
+        if (block.chainid == LOCAL_CHAIN_ID) {
+            vm.startBroadcast();
+            VRFCoordinatorV2_5Mock(vrfCoordinator).fundSubscription(
+                subscriptionId,
+                FUND_AMOUNT * 50
+            );
+            vm.stopBroadcast();
+        } else {
+            vm.startBroadcast();
+            LinkToken(linkToken).transferAndCall(
+                vrfCoordinator,
+                FUND_AMOUNT,
+                abi.encode(subscriptionId)
+            );
+            vm.stopBroadcast();
+        }
+    }
+
+    function run() public {}
+}
+
+contract AddConsumer is Script {
+    function addConsumerUsingConfig(address mostRecentDeployment) public {
+        HelperConfig helperConfig = new HelperConfig();
+        uint256 subId = helperConfig.getConfig().subscriptionId;
+        address vrfCoordinator = helperConfig.getConfig().vrfCoordinator;
+        addConsumer(mostRecentDeployment, vrfCoordinator, subId);
+    }
+
+    function addConsumer(
+        address contractToAddToVrf,
+        address vrfCoordinator,
+        uint256 subId
+    ) public {
+        console.log("Adding VRF consumer to contract: ", contractToAddToVrf);
+        console.log("Using subscription ID: ", subId);
+        console.log("On ChainId: ", block.chainid);
+
+        vm.startBroadcast();
+        VRFCoordinatorV2_5Mock(vrfCoordinator).addConsumer(
+            subId,
+            contractToAddToVrf
+        );
+        vm.stopBroadcast();
+    }
+
+    function run() public {
+        address mostRecentDeployment = DevOpsTools.get_most_recent_deployment(
+            "Raffle",
+            block.chainid
+        );
+        addConsumerUsingConfig(mostRecentDeployment);
+    }
+}
